4866ed7c1f0db9c9ef38ac1e805a8db5f7ee85f7
dariuslacatus03 <dariusandrey2003@gmail.com>
2024-03-28 21:42:56 +0200

    solved last requirement

--- University Projects/Databases Management Systems/Lab1/lab_01/lab_01/Form1.Designer.cs
+++ University Projects/Databases Management Systems/Lab1/lab_01/lab_01/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button2 = new System.Windows.Forms.Button();
             this.speciesGrid = new System.Windows.Forms.DataGridView();
             this.speciesNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mediumSizeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -56,6 +55,22 @@
             this.speciesTableAdapter = new lab_01.FishingAppDataSetTableAdapters.SpeciesTableAdapter();
             this.fishTableAdapter = new lab_01.FishingAppDataSetTableAdapters.FishTableAdapter();
             this.fishBySpeciesGrid = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.fishIdTextBox = new System.Windows.Forms.TextBox();
+            this.fishIdLabel = new System.Windows.Forms.Label();
+            this.userIdLabel = new System.Windows.Forms.Label();
+            this.userIdTextBox = new System.Windows.Forms.TextBox();
+            this.locationLabel = new System.Windows.Forms.Label();
+            this.locationTextBox = new System.Windows.Forms.TextBox();
+            this.riverLabel = new System.Windows.Forms.Label();
+            this.riverTextBox = new System.Windows.Forms.TextBox();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.sizeTextBox = new System.Windows.Forms.TextBox();
+            this.speciesIdLabel = new System.Windows.Forms.Label();
+            this.speciesNameTextBox = new System.Windows.Forms.TextBox();
+            this.allFishTableAdapter1 = new lab_01.FishingAppDataSetTableAdapters.AllFishTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.speciesGrid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.speciesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fishingAppDataSetBindingSource)).BeginInit();
@@ -68,16 +83,6 @@
             this.fillFishesToolStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fishBySpeciesGrid)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(263, 272);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(8, 7);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // speciesGrid
             // 
@@ -143,9 +148,14 @@
             this.fillSpeciesToolStripButton});
             this.fillSpeciesToolStrip.Location = new System.Drawing.Point(0, 0);
             this.fillSpeciesToolStrip.Name = "fillSpeciesToolStrip";
-            this.fillSpeciesToolStrip.Size = new System.Drawing.Size(1452, 27);
+            this.fillSpeciesToolStrip.Size = new System.Drawing.Size(1452, 25);
             this.fillSpeciesToolStrip.TabIndex = 3;
             this.fillSpeciesToolStrip.Text = "fillSpeciesToolStrip";
+            // 
+            // fillSpeciesToolStripButton
+            // 
+            this.fillSpeciesToolStripButton.Name = "fillSpeciesToolStripButton";
+            this.fillSpeciesToolStripButton.Size = new System.Drawing.Size(29, 22);
             // 
             // fishGrid
             // 
@@ -159,7 +169,7 @@
             this.riverDataGridViewTextBoxColumn,
             this.locationNameDataGridViewTextBoxColumn});
             this.fishGrid.DataSource = this.fishBindingSource;
-            this.fishGrid.Location = new System.Drawing.Point(15, 316);
+            this.fishGrid.Location = new System.Drawing.Point(15, 392);
             this.fishGrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.fishGrid.Name = "fishGrid";
             this.fishGrid.RowHeadersWidth = 51;
@@ -225,11 +235,16 @@
             this.fillFishesToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.fillFishesToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fillFishesToolStripButton});
-            this.fillFishesToolStrip.Location = new System.Drawing.Point(0, 27);
+            this.fillFishesToolStrip.Location = new System.Drawing.Point(0, 25);
             this.fillFishesToolStrip.Name = "fillFishesToolStrip";
-            this.fillFishesToolStrip.Size = new System.Drawing.Size(1452, 27);
+            this.fillFishesToolStrip.Size = new System.Drawing.Size(1452, 25);
             this.fillFishesToolStrip.TabIndex = 5;
             this.fillFishesToolStrip.Text = "fillFishesToolStrip";
+            // 
+            // fillFishesToolStripButton
+            // 
+            this.fillFishesToolStripButton.Name = "fillFishesToolStripButton";
+            this.fillFishesToolStripButton.Size = new System.Drawing.Size(29, 22);
             // 
             // championshipTableAdapter
             // 
@@ -256,18 +271,166 @@
             this.fishBySpeciesGrid.RowTemplate.Height = 24;
             this.fishBySpeciesGrid.Size = new System.Drawing.Size(583, 199);
             this.fishBySpeciesGrid.TabIndex = 6;
+            this.fishBySpeciesGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.fishBySpeciesGrid_CellClick);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(15, 313);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Add Fish";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(96, 313);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(92, 23);
+            this.updateButton.TabIndex = 8;
+            this.updateButton.Text = "Update Fish";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(194, 313);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(99, 23);
+            this.removeButton.TabIndex = 9;
+            this.removeButton.Text = "Remove Fish";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // fishIdTextBox
+            // 
+            this.fishIdTextBox.Location = new System.Drawing.Point(465, 295);
+            this.fishIdTextBox.Name = "fishIdTextBox";
+            this.fishIdTextBox.Size = new System.Drawing.Size(78, 22);
+            this.fishIdTextBox.TabIndex = 10;
+            this.fishIdTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.fishIdTextBox_KeyPress);
+            // 
+            // fishIdLabel
+            // 
+            this.fishIdLabel.AutoSize = true;
+            this.fishIdLabel.Location = new System.Drawing.Point(408, 298);
+            this.fishIdLabel.Name = "fishIdLabel";
+            this.fishIdLabel.Size = new System.Drawing.Size(51, 16);
+            this.fishIdLabel.TabIndex = 11;
+            this.fishIdLabel.Text = "Fish ID:";
+            // 
+            // userIdLabel
+            // 
+            this.userIdLabel.AutoSize = true;
+            this.userIdLabel.Location = new System.Drawing.Point(603, 297);
+            this.userIdLabel.Name = "userIdLabel";
+            this.userIdLabel.Size = new System.Drawing.Size(55, 16);
+            this.userIdLabel.TabIndex = 13;
+            this.userIdLabel.Text = "User ID:";
+            // 
+            // userIdTextBox
+            // 
+            this.userIdTextBox.Location = new System.Drawing.Point(664, 294);
+            this.userIdTextBox.Name = "userIdTextBox";
+            this.userIdTextBox.Size = new System.Drawing.Size(78, 22);
+            this.userIdTextBox.TabIndex = 12;
+            this.userIdTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.userIdTextBox_KeyPress);
+            // 
+            // locationLabel
+            // 
+            this.locationLabel.AutoSize = true;
+            this.locationLabel.Location = new System.Drawing.Point(800, 331);
+            this.locationLabel.Name = "locationLabel";
+            this.locationLabel.Size = new System.Drawing.Size(61, 16);
+            this.locationLabel.TabIndex = 15;
+            this.locationLabel.Text = "Location:";
+            // 
+            // locationTextBox
+            // 
+            this.locationTextBox.Location = new System.Drawing.Point(867, 328);
+            this.locationTextBox.Name = "locationTextBox";
+            this.locationTextBox.Size = new System.Drawing.Size(78, 22);
+            this.locationTextBox.TabIndex = 14;
+            // 
+            // riverLabel
+            // 
+            this.riverLabel.AutoSize = true;
+            this.riverLabel.Location = new System.Drawing.Point(819, 298);
+            this.riverLabel.Name = "riverLabel";
+            this.riverLabel.Size = new System.Drawing.Size(42, 16);
+            this.riverLabel.TabIndex = 17;
+            this.riverLabel.Text = "River:";
+            // 
+            // riverTextBox
+            // 
+            this.riverTextBox.Location = new System.Drawing.Point(867, 295);
+            this.riverTextBox.Name = "riverTextBox";
+            this.riverTextBox.Size = new System.Drawing.Size(78, 22);
+            this.riverTextBox.TabIndex = 16;
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(622, 331);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(36, 16);
+            this.sizeLabel.TabIndex = 19;
+            this.sizeLabel.Text = "Size:";
+            // 
+            // sizeTextBox
+            // 
+            this.sizeTextBox.Location = new System.Drawing.Point(664, 328);
+            this.sizeTextBox.Name = "sizeTextBox";
+            this.sizeTextBox.Size = new System.Drawing.Size(78, 22);
+            this.sizeTextBox.TabIndex = 18;
+            this.sizeTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sizeTextBox_KeyPress);
+            // 
+            // speciesIdLabel
+            // 
+            this.speciesIdLabel.AutoSize = true;
+            this.speciesIdLabel.Location = new System.Drawing.Point(359, 331);
+            this.speciesIdLabel.Name = "speciesIdLabel";
+            this.speciesIdLabel.Size = new System.Drawing.Size(100, 16);
+            this.speciesIdLabel.TabIndex = 21;
+            this.speciesIdLabel.Text = "Species Name:";
+            // 
+            // speciesNameTextBox
+            // 
+            this.speciesNameTextBox.Location = new System.Drawing.Point(465, 328);
+            this.speciesNameTextBox.Name = "speciesNameTextBox";
+            this.speciesNameTextBox.Size = new System.Drawing.Size(78, 22);
+            this.speciesNameTextBox.TabIndex = 20;
+            // 
+            // allFishTableAdapter1
+            // 
+            this.allFishTableAdapter1.ClearBeforeFill = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1452, 576);
+            this.ClientSize = new System.Drawing.Size(1452, 630);
+            this.Controls.Add(this.speciesIdLabel);
+            this.Controls.Add(this.speciesNameTextBox);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.sizeTextBox);
+            this.Controls.Add(this.riverLabel);
+            this.Controls.Add(this.riverTextBox);
+            this.Controls.Add(this.locationLabel);
+            this.Controls.Add(this.locationTextBox);
+            this.Controls.Add(this.userIdLabel);
+            this.Controls.Add(this.userIdTextBox);
+            this.Controls.Add(this.fishIdLabel);
+            this.Controls.Add(this.fishIdTextBox);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
             this.Controls.Add(this.fishBySpeciesGrid);
             this.Controls.Add(this.fillFishesToolStrip);
             this.Controls.Add(this.fishGrid);
             this.Controls.Add(this.fillSpeciesToolStrip);
             this.Controls.Add(this.speciesGrid);
-            this.Controls.Add(this.button2);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -318,6 +481,22 @@
         private System.Windows.Forms.ToolStrip fillFishesToolStrip;
         private System.Windows.Forms.ToolStripButton fillFishesToolStripButton;
         private System.Windows.Forms.DataGridView fishBySpeciesGrid;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.TextBox fishIdTextBox;
+        private System.Windows.Forms.Label fishIdLabel;
+        private System.Windows.Forms.Label userIdLabel;
+        private System.Windows.Forms.TextBox userIdTextBox;
+        private System.Windows.Forms.Label locationLabel;
+        private System.Windows.Forms.TextBox locationTextBox;
+        private System.Windows.Forms.Label riverLabel;
+        private System.Windows.Forms.TextBox riverTextBox;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.TextBox sizeTextBox;
+        private System.Windows.Forms.Label speciesIdLabel;
+        private System.Windows.Forms.TextBox speciesNameTextBox;
+        private FishingAppDataSetTableAdapters.AllFishTableAdapter allFishTableAdapter1;
     }
 }
 
--- University Projects/Databases Management Systems/Lab1/lab_01/lab_01/Form1.cs
+++ University Projects/Databases Management Systems/Lab1/lab_01/lab_01/Form1.cs
@@ -26,6 +26,7 @@ namespace lab_01
         {
             this.fillSpecies();
             this.fillFish();
+
         }
 
 
@@ -68,21 +69,201 @@ namespace lab_01
 
         }
 
+        private void fillFishBySpeciesGrid(string speciesName)
+        {
+            try
+            {
+                string sqlCommand = "select * from Fish where SpeciesName=@SpeciesName";
+                SqlCommand getFishBySpecies = new SqlCommand(sqlCommand, connection);
+                getFishBySpecies.Parameters.AddWithValue("@SpeciesName", speciesName);
+
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(getFishBySpecies);
+
+                sqlDataAdapter.Fill(dataSet, "Fish");
+                fishBySpeciesGrid.DataSource = dataSet.Tables["Fish"];
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
         private void speciesGrid_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            var speciesID = speciesGrid.Rows[e.RowIndex].Cells[0].Value;
+            string speciesName = Convert.ToString(speciesGrid.Rows[e.RowIndex].Cells[0].Value);
 
-            string sqlCommand = "SELECT * FROM FISH WHERE SpeciesName=@SpeciesID";
-
-            SqlCommand getFishBySpecies = new SqlCommand(sqlCommand, connection);
-            getFishBySpecies.Parameters.AddWithValue("@SpeciesID", speciesID);
-
-            DataSet dataSet = new DataSet();
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(getFishBySpecies);
-
-            dataAdapter.Fill(dataSet, "Fish");
-            fishBySpeciesGrid.DataSource = dataSet.Tables["Fish"];
+            fillFishBySpeciesGrid(speciesName);
 
         }
+
+        private void fishBySpeciesGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                string fishID = Convert.ToString(fishBySpeciesGrid.Rows[e.RowIndex].Cells[0].Value);
+                string size = Convert.ToString(fishBySpeciesGrid.Rows[e.RowIndex].Cells[1].Value);
+                string speciesName = Convert.ToString(fishBySpeciesGrid.Rows[e.RowIndex].Cells[2].Value);
+                string userID = Convert.ToString(fishBySpeciesGrid.Rows[e.RowIndex].Cells[3].Value);
+                string river = Convert.ToString(fishBySpeciesGrid.Rows[e.RowIndex].Cells[4].Value);
+                string location = Convert.ToString(fishBySpeciesGrid.Rows[e.RowIndex].Cells[5].Value);
+
+                fishIdTextBox.Text = fishID;
+                sizeTextBox.Text = size;
+                speciesNameTextBox.Text = speciesName;
+                userIdTextBox.Text = userID;
+                riverTextBox.Text = river;
+                locationTextBox.Text = location;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+
+        private void fishIdTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void userIdTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void sizeTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int fishID = Convert.ToInt32(fishIdTextBox.Text);
+                double size = Convert.ToDouble(sizeTextBox.Text);
+                string speciesName = speciesNameTextBox.Text;
+                int userID = Convert.ToInt32(userIdTextBox.Text);
+                string river = riverTextBox.Text;
+                string locationName = locationTextBox.Text;
+
+
+                string sqlCommand = "INSERT INTO Fish (FishID, Size, SpeciesName, UserID, River, LocationName) " +
+                                    "VALUES (@FishID, @Size, @SpeciesName, @UserID, @River, @LocationName)";
+                SqlCommand addFishCommand = new SqlCommand(sqlCommand, connection);
+                addFishCommand.Parameters.AddWithValue("@FishID", fishID);
+                addFishCommand.Parameters.AddWithValue("@Size", size);
+                addFishCommand.Parameters.AddWithValue("@SpeciesName", speciesName);
+                addFishCommand.Parameters.AddWithValue("@UserID", userID);
+                addFishCommand.Parameters.AddWithValue("@River", river);
+                addFishCommand.Parameters.AddWithValue("@LocationName", locationName);
+
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(addFishCommand);
+
+                dataAdapter.Fill(dataSet, "Fish");
+                fishGrid.DataSource = dataSet.Tables["Fish"];
+
+                fillFish();
+                fillFishBySpeciesGrid(speciesName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding fish record: " + ex.Message);
+            }
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (fishBySpeciesGrid.CurrentCell == null)
+                {
+                    MessageBox.Show("Please select a fish record to remove.");
+                    return; // Exit the method if no row is selected
+                }
+
+                int rowIndex = fishBySpeciesGrid.CurrentCell.RowIndex;
+                int fishID = Convert.ToInt32(fishBySpeciesGrid.Rows[rowIndex].Cells["FishID"].Value);
+                string speciesName = Convert.ToString(fishBySpeciesGrid.Rows[rowIndex].Cells["SpeciesName"].Value);
+                // Delete the fish record from the Fish table
+                string sqlCommand = "DELETE FROM Fish WHERE FishID = @FishID";
+                SqlCommand removeFishCommand = new SqlCommand(sqlCommand, connection);
+                removeFishCommand.Parameters.AddWithValue("@FishID", fishID);
+
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(removeFishCommand);
+
+                dataAdapter.Fill(dataSet, "Fish");
+                fishGrid.DataSource = dataSet.Tables["Fish"];
+                fishBySpeciesGrid.DataSource = dataSet.Tables["Fish"];
+
+                // Refresh the fish data grid after removing the fish record
+                fillFish();
+                fillFishBySpeciesGrid(speciesName);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error removing fish record: " + ex.Message);
+            }
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int fishID = Convert.ToInt32(fishIdTextBox.Text);
+                double size = Convert.ToDouble(sizeTextBox.Text);
+                string speciesName = speciesNameTextBox.Text;
+                int userID = Convert.ToInt32(userIdTextBox.Text);
+                string river = riverTextBox.Text;
+                string locationName = locationTextBox.Text;
+
+                // Update the fish record in the Fish table
+                string sqlCommand = "UPDATE Fish SET FishID = @FishID, Size = @Size, SpeciesName = @SpeciesName," +
+                                    "UserID = @UserID, River = @River, LocationName = @LocationName WHERE FishID = @FishID";
+                SqlCommand updateFishCommand = new SqlCommand(sqlCommand, connection);
+                updateFishCommand.Parameters.AddWithValue("@FishID", fishID);
+                updateFishCommand.Parameters.AddWithValue("@Size", size);
+                updateFishCommand.Parameters.AddWithValue("@SpeciesName", speciesName);
+                updateFishCommand.Parameters.AddWithValue("@UserID", userID);
+                updateFishCommand.Parameters.AddWithValue("@River", river);
+                updateFishCommand.Parameters.AddWithValue("@LocationName", locationName);
+
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(updateFishCommand);
+
+                dataAdapter.Fill(dataSet, "Fish");
+                fishGrid.DataSource = dataSet.Tables["Fish"];
+                fishBySpeciesGrid.DataSource = dataSet.Tables["Fish"];
+
+                // Refresh the fish data grid after updating the fish record
+                fillFish();
+                fillFishBySpeciesGrid(speciesName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating fish record: " + ex.Message);
+            }
+        }
+
+        
     }
 }
